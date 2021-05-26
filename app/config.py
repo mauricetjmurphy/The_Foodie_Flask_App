4451ecd40bdaa4e8d22f95b9b3d698809755cb4c
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-05-26 08:27:45 +0100

    Created a config class in the config.py file, to store all config settings for the app

---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+
+#Config object to store to staore all config settings for the app
+class Config(object):
+    SECRET_KEY =  '\xac\xa0\xcd\xeb\x97y\x97|\x136\xa1\xee\x8b\x07\x18\x9d'
+
+    MONGODB_SETTINGS = {"db" : "Recipe_App"}
