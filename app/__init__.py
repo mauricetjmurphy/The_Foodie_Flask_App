17c6daef8416a67dcbf1510ce15b757f56d27597
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-05-26 17:42:20 +0100

    Configured the app object and instantiated the mongoengine object

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 # Import the required modules
 from flask import Flask
+from app.config import Config
+from flask_mongoengine import MongoEngine
 
 # Instantiate the flask app object
 app = Flask(__name__)
+# Configure the app object using the config class
+app.config.from_object(Config)
+
+# Instantiate the mongo engine object
+db = MongoEngine()
+db.init_app(app)
 
 # Import poutes below the app instantiation to prevent circular imports
 from app import routes
