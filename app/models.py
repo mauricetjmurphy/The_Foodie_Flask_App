00f66030dcfb364df5c4084e560894ff535f1756
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-05-27 10:24:15 +0100

    Registration template created with all flask-wtf fields added and also error handling.

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class User(db.Document, UserMixin):
         return True
 
     def get_id(self):
-        """Return the email address to satisfy Flask-Login's requirements."""
+        """Return the user id to satisfy Flask-Login's requirements."""
         return self.user_id
 
     def is_authenticated(self):
