5fd454c807e1694eff5cca3bf5fcb9f0c617f869
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-06-12 22:44:17 +0100

    Removed the action attribute from the form. Flask-wtf does not require it and it also causes issues

--- app/routes.py
+++ app/routes.py
@@ -313,11 +313,11 @@ def contact_form():
     return redirect(url_for('contact'))
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('errors/404.html'), 404
+# @app.errorhandler(404)
+# def not_found(error):
+#     return render_template('errors/404.html'), 404
 
 
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('errors/500.html'), 500
+# @app.errorhandler(500)
+# def internal_error(error):
+#     return render_template('errors/500.html'), 500
