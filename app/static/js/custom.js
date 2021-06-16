e5fab1bf95fc7649c7a8ecb98102aaee7ce6f4e3
mmurphy-1 <64709774+mmurphy-1@users.noreply.github.com>
2021-06-16 20:04:48 +0100

    Updated the recipe form validation. Switched from flask-wtf validation for javascript validation because it was not possible to validate some of the inputs

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
     value=""
     type="text"
     name="ingredient-field[]"
-    class="ingredient_input"
-    placeholder="Ingredient *"
+    class="ingredient_input mb-3"
+    placeholder="Ingredient"
     required
 />`;
     $(add_input).click(function () {
@@ -42,7 +42,7 @@ $(document).ready(function () {
     type="text"
     name="step-field[]"
     class="step_input"
-    placeholder="Step *"
+    placeholder="Step"
     required
 ></textarea>`;
     $(add_input).click(function () {
@@ -59,4 +59,105 @@ $(document).ready(function () {
     $(add_input).click(function () {
         $(input_wrapper).children().last().remove();
     });
+});
+
+// Element selectors for the recipe form
+const dishImage = document.querySelector("#dishImage");
+const recipeTitle = document.querySelector("#recipeTitle");
+const description = document.querySelector("#description");
+const category = document.querySelector("#category");
+const ingredient = document.querySelector("#ingredient");
+const step = document.querySelector("#step");
+const recipeForm = document.getElementById("recipeForm");
+
+//Adding an event listener on the form submit. Conditional if/else statement validate the form
+recipeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    isFormValid = true;
+
+    if (!dishImage.value) {
+        const error = dishImage.parentElement.querySelector(".error-message");
+        error.classList.remove("hidden");
+        dishImage.classList.remove("mb-4");
+        dishImage.classList.add("mb-0");
+        dishImage.scrollIntoView();
+
+        isFormValid = false;
+    } else {
+        const error = dishImage.parentElement.querySelector(".error-message");
+        error.classList.add("hidden");
+        dishImage.classList.add("mb-4");
+    }
+    if (!recipeTitle.value) {
+        const error = recipeTitle.parentElement.querySelector(".error-message");
+        error.classList.remove("hidden");
+        recipeTitle.classList.remove("mb-4");
+        recipeTitle.classList.add("mb-0");
+        dishImage.scrollIntoView();
+
+        isFormValid = false;
+    } else {
+        const error = recipeTitle.parentElement.querySelector(".error-message");
+        error.classList.add("hidden");
+        recipeTitle.classList.add("mb-4");
+    }
+    if (description.value.length < 10) {
+        const error = description.parentElement.querySelector(".error-message");
+        error.classList.remove("hidden");
+        const min = description.parentElement.querySelector("#min-char");
+        min.classList.remove("mb-4");
+        min.classList.add("mb-0");
+        dishImage.scrollIntoView();
+
+        isFormValid = false;
+    } else {
+        const error = description.parentElement.querySelector(".error-message");
+        error.classList.add("hidden");
+        const min = description.parentElement.querySelector("#min-char");
+        min.classList.add("mb-4");
+    }
+    if (category.value.length < 1) {
+        const error = category.parentElement.querySelector(".error-message");
+        error.classList.remove("hidden");
+        category.classList.remove("mb-4");
+        category.classList.add("mb-0");
+        dishImage.scrollIntoView();
+
+        isFormValid = false;
+    } else {
+        const error = category.parentElement.querySelector(".error-message");
+        error.classList.add("hidden");
+        category.classList.add("mb-4");
+    }
+    if (!ingredient.value) {
+        const error = ingredient.parentElement.querySelector(".error-message");
+        error.classList.remove("hidden");
+        ingredient.classList.remove("mb-4");
+        ingredient.classList.add("mb-0");
+        dishImage.scrollIntoView();
+
+        isFormValid = false;
+    } else {
+        const error = ingredient.parentElement.querySelector(".error-message");
+        error.classList.add("hidden");
+        ingredient.classList.add("mb-4");
+    }
+    if (!step.value) {
+        const error = step.parentElement.querySelector(".error-message");
+        error.classList.remove("hidden");
+        step.classList.remove("mb-4");
+        step.classList.add("mb-0");
+        dishImage.scrollIntoView();
+
+        isFormValid = false;
+    } else {
+        const error = step.parentElement.querySelector(".error-message");
+        error.classList.add("hidden");
+        step.classList.add("mb-4");
+    }
+
+    if (isFormValid) {
+        recipeForm.submit();
+    }
 });
